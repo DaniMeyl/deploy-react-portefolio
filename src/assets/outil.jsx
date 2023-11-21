596fd8c3403dc7768b744927b2b05411e56054b2
Dani Meylemans <dani.meylemans@gmail.com>
2023-11-21 17:18:40 +0100

    fixe some details css

---
+++ src/assets/outil.jsx
@@ -0,0 +1,52 @@
+const data = 
+{
+    "outils": [
+      {
+          "title": "Formation",
+          "items": [
+            {
+              "icon": require("./skills/openclassrooms.png"),
+              "title": "Developpeur Front-end React"
+            }
+          ]
+        },
+      {
+        "title": "Langages & Bases de données",
+        "items": [
+          {
+            "icon": require("./skills/html.png"),
+            "title": "HTML"
+          },
+          {
+            "icon": require("./skills/css.png"),
+            "title": "CSS"
+          },
+          {
+            "icon": require("./skills/js.png"),
+            "title": "JavaScript"
+          },
+          {
+            "icon": require( "./skills/mongo-db.png"),
+            "title": "MongoDb"
+          }
+        ]
+      },
+    
+      {
+          "title": "Frameworks & Outils",
+          "items": [
+            {
+              "icon": require("./skills/react.png"),
+              "title": "React"
+            },
+    
+            {
+              "icon": require("./skills/git.png"),
+              "title": "Git"
+            }
+          ]
+        }
+    ]
+  }
+  export default data
+  
